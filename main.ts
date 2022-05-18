7bd46bb383c888de45726935a3425f97b1cccd0d
martinmcy <104663090+martinmcy@users.noreply.github.com>
2022-05-18 13:13:30 +0800

    還原「2022/5/9 下午7:21:52 m」

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 basic.forever(function () {
-    basic.showNumber(1)
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.showIcon(IconNames.Tortoise)
+    basic.showLeds(`
+        . . # # #
+        # # . # .
+        . # # # .
+        . # # # .
+        . . . # .
+        `)
 })
